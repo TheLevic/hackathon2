981be0348bd740e862c01e46dc8d8af3fe3c92a8
TheLevic <levicrider08@gmail.com>
2021-11-20 13:36:56 -0600

    Working Product

--- test.cpp
+++ test.cpp
@@ -6,6 +6,112 @@
 #include <string>
 #include <fstream>
 using namespace std;
+
+//global variable main 8 allergies
+bool Milk = false;
+bool Eggs = false;
+bool Fish = false;
+bool Shellfish = false;
+bool Treenuts = false;
+bool Peanuts = false;
+bool Wheat = false;
+bool Soybean = false;
+bool Sesame = false;
+
+void userSetup(){
+  string userName;
+  cout << "Hi, welcome to Allergens!\nWhat's your name?\n";
+  cin >> userName;
+  cout << "Nice to meet you "+userName+"!\n";
+  cout << "Which of the following food allergies do you have?\n";
+  cout << "Milk\nEggs\nFish\nShellfish\nTreenuts\nPeanuts\nWheat\nSoybean\nSesame\n";
+  cout << "Input directly as displayed\n";
+  string allergies;
+  getline(cin, allergies);
+  
+  getline(cin, allergies);
+  
+  const char *allergens[9] = { "Milk", "Eggs","Fish", "Shellfish","Treenuts", "Peanuts", "Wheat", "Soybean", "Sesame"};
+                             
+  for (int i = 0; i<9;i++)
+  {
+    if (allergies.find(allergens[i]) != string::npos){
+      if(i==0){Milk=true;}
+      else if(i==1){Eggs=true;}
+      else if(i==2){Fish=true;}
+      else if(i==3){Shellfish=true;}
+      else if(i==4){Treenuts=true;}
+      else if(i==5){Peanuts=true;}
+      else if(i==6){Wheat=true;}
+      else if(i==7){Soybean=true;}
+      else if(i==8){Sesame=true;}
+    }
+  }
+
+  /*cout << Milk<< Eggs << Fish<< Shellfish<< Treenuts<< Peanuts<< Wheat<< Soybean<< Sesame<<"\n";*/
+
+  return;
+}
+
+void checkComp(std::string allergen){
+  if(Milk){
+      if(allergen[0]=='1'){
+      cout << "May contain traces of Milk\n\n";}
+      if(allergen[0]=='2')
+      {cout << "Contains Milk\n\n";}
+    }
+ if(Eggs){
+  if(allergen[1]=='1')
+      {cout << "May contain traces of Eggs\n\n";}
+      if(allergen[1]=='2')
+      {cout << "Contains Eggs\n\n";}
+  }
+ if(Fish){
+  if(allergen[2]=='1')
+      {cout << "May contain traces of Fish\n\n";}
+      if(allergen[2]=='2')
+      {cout << "Contains Fish\n\n";}
+  }
+  
+ if(Shellfish){
+  if(allergen[3]=='1')
+      {cout << "May contain traces of Shellfish\n\n";}
+      if(allergen[3]=='2')
+      {cout << "Contains Shellfish\n\n";}
+      }
+ if(Treenuts){
+  if(allergen[4]=='1')
+      {cout << "May contain traces of Treenuts\n\n";}
+      if(allergen[4]=='2')
+      {cout << "Contains Treenuts\n\n";}
+      }
+ if(Peanuts){
+  if(allergen[5]=='1')
+      {cout << "May contain traces of Peanuts\n\n";}
+      if(allergen[5]=='2')
+      {cout << "Contains\n\n";}
+      }
+ if(Wheat){
+  if(allergen[6]=='1')
+      {cout << "May contain traces of Wheat \n\n";}
+      if(allergen[6]=='2')
+      {cout << "Contains Wheat \n\n";}
+      }
+ if(Soybean){
+  if(allergen[7]=='1')
+      {cout << "May contain traces of Soybean\n\n";}
+      if(allergen[7]=='2')
+      {cout << "Contains Soybean\n\n";}
+      }
+ if(Sesame){
+  if(allergen[8]=='1')
+      {cout << "May contain traces of Sesame\n\n";}
+      if(allergen[8]=='2')
+      {cout << "Contains Sesame\n\n";}
+  }
+}
+
+
 static int callback(void *data, int argc, char **argv, char **azColName){
    int i;
 ofstream file;
@@ -22,10 +128,10 @@ int main(int argc, char* argv[]) {
    bool run;
    string allergies;
    char *choice = new char[20];
+   string allergyList;
 
    //Getting data from user
-   cout << "What are your food allergies: ";
-   cin >> allergies;
+   userSetup();
    cout << "What would you like to eat today? " << endl
         << "1. Snickers" << endl
         << "2. Doritos" << endl
@@ -39,6 +145,7 @@ int main(int argc, char* argv[]) {
         << "10. Oreos" << endl;
 
    cin >> choice;
+
 
 
 
@@ -82,10 +189,7 @@ int main(int argc, char* argv[]) {
           allergen.append(1,fromFile[i]);
       }
    }
-   cout << allergen;
-
-
-   
+   checkComp(allergen);
 return 0;
 }
 
